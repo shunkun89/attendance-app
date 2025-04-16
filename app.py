1cc3b2865aa8dbd5ebeb08c883187b917cd45a4d
shunkun89 <shunkun4589@gmail.com>
2025-04-16 14:37:53 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def index():
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
                  "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
-        creds = ServiceAccountCredentials.from_json_keyfile_name('attendance-sheet-456907-814285e7669c.json', scope)
+        creds = ServiceAccountCredentials.from_json_keyfile_name('attendance-sheet-456907-b2d0c5cb777a.json', scope)
 
         client = gspread.authorize(creds)
 
